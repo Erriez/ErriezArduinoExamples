043603096391f633d32a8febb1ea091f58bc1003
Erriez <Erriez@users.noreply.github.com>
2020-09-12 20:43:23 +0200

    Simplify ESP8266 NTP example

--- examples/Processors/ESP8266/WiFi/ESP8266_NTP/ESP8266_NTP.ino
+++ examples/Processors/ESP8266/WiFi/ESP8266_NTP/ESP8266_NTP.ino
@@ -23,82 +23,141 @@
  */
 
 /*!
- * \brief Erriez ESP8266 NTP example
+ * \brief Erriez ESP8266 NTP UTC example
  * \details
  *      Source:         https://github.com/Erriez/ErriezArduinoExamples
  */
- 
+
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <Udp.h>
 #include <time.h>
 
 // SSID and Password of your WiFi router
-const char *ssid = "***";
-const char *password = "***";    
+#define WIFI_SSID           ""
+#define WIFI_PASSWORD       ""
+
+// "pool.ntp.org", "time.nist.gov" or IP address
+#define NTP_SERVER          "pool.ntp.org"
+// Receive timeout
+#define NTP_RX_TIMEOUT      2000
+
+// NTP time stamp is in the first 48 bytes of the message
+#define NTP_PACKET_SIZE     48
+// UDP listen port
+#define LOCAL_PORT          2390
+
+// WiFi UDP object
+WiFiUDP udp;
 
 
-void getNTP()
+void initWifi()
 {
-    struct tm timeinfo;
-    time_t now;
-    char line[40];
+    Serial.print(F("Connecting to '"));
+    Serial.print(WIFI_SSID);
+    Serial.print(F("'"));
 
-    // Configure NTP servers
-    configTime(1 * 3600, 0, "pool.ntp.org", "time.nist.gov");
-
-    Serial.print(F("Waiting for NTP time sync"));
-    now = time(nullptr);
-    while (now < (8 * 3600 * 2)) {
-        delay(500);
-        Serial.print(".");
-        now = time(nullptr);
-    }
-    Serial.println("");
-
-    // Convert time_t to tm as UTC time
-    gmtime_r(&now, &timeinfo);
-
-    // Format time as string
-    strftime(line, sizeof(line), "%A %d %B %Y  %H:%M:%S", &timeinfo);
-
-    // Print date/time
-    Serial.print(F("Epoch: "));
-    Serial.println(now);
-    Serial.print(F("Current time: "));
-    Serial.println(line);
-}
-
-void wifiConnect()
-{
-    Serial.printf_P(PSTR("Connected to %s"), ssid);
-    
     // Connect to your WiFi router
-    WiFi.begin(ssid, password);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     // Wait for connection
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
-    Serial.println(F("OK"));
+
+    // If connection successful show IP address in serial monitor
+    Serial.println("OK");
+}
+
+void sendNTPpacket(const char *ntpServer)
+{
+    static bool initialized  = false;
+    uint8_t txBuffer[NTP_PACKET_SIZE];
+
+    // Initialize UDP
+    if (!initialized) {
+        udp.begin(LOCAL_PORT);
+        initialized = true;
+    }
+
+    // Clear transmit buffer
+    memset(txBuffer, 0, NTP_PACKET_SIZE);
+
+    // Initialize values needed to form NTP request
+    txBuffer[0] = 0b11100011; // LI, Version, Mode
+    txBuffer[1] = 0;          // Stratum, or type of clock
+    txBuffer[2] = 6;          // Polling Interval
+    txBuffer[3] = 0xEC;       // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    txBuffer[12]  = 49;
+    txBuffer[13]  = 0x4E;
+    txBuffer[14]  = 49;
+    txBuffer[15]  = 52;
+
+    // Send NTP packet to port 123
+    udp.beginPacket(ntpServer, 123);
+    udp.write(txBuffer, NTP_PACKET_SIZE);
+    udp.endPacket();
+}
+
+time_t getEpoch(const char *ntpServer, uint16_t timeoutMs=1000)
+{
+    static uint32_t tPacketSent = 0;
+    uint8_t rxBuffer[NTP_PACKET_SIZE];
+    time_t t = 0;
+
+    // Send packet
+    tPacketSent = millis();
+    sendNTPpacket(ntpServer);
+
+    do {
+        // Check if packet received
+        if (udp.parsePacket()) {
+            // Read data from received UDP packet
+            udp.read(rxBuffer, NTP_PACKET_SIZE);
+
+
+            // NTP time in seconds since Jan 1 1900
+            t = (rxBuffer[40] << 24) | (rxBuffer[41] << 16) |
+                (rxBuffer[42] << 8) | (rxBuffer[43] << 0);
+
+            // Unix time starts on Jan 1 1970. In seconds, that's 2208988800
+            t -= 2208988800UL;
+
+            break;
+        }
+    } while ((millis() - tPacketSent) < timeoutMs);
+
+    return t;
 }
 
 void setup()
 {
-    // Startup delay to initialize serial port
-    delay(500);
-    
+    time_t t;
+
     // Initialize serial
+    delay(500);
     Serial.begin(115200);
-    Serial.println(F("\nESP8266 NTP example"));
+    Serial.println(F("\nErriez ESP8266 NTP example"));
 
-    // WiFi connect
-    wifiConnect();
+    // Initialize WiFi
+    initWifi();
 
-    // Get NTP
-    getNTP();
+    // Get epoch
+    t = getEpoch(NTP_SERVER, NTP_RX_TIMEOUT);
+
+    // Print result
+    if (t > 0) {
+        Serial.print(F("Epoch: "));
+        Serial.println((uint32_t)t);
+        Serial.print(F("UTC:   "));
+        Serial.println(ctime(&t));
+    } else {
+        Serial.println(F("Timeout"));
+    }
 }
 
 void loop()
 {
-
 }
